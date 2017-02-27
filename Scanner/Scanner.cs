80306b91fbcae4d82e13422261e42b9ef26a1653
MaxKlaxxMiner <max@tetrisk.de>
2017-02-27 18:33:10 +0100

    create Scanner-project

---
+++ Scanner/Scanner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scanner
+{
+  public partial class Scanner : Form
+  {
+    public Scanner()
+    {
+      InitializeComponent();
+    }
+  }
+}
