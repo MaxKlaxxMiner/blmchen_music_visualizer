31f9d52e9a60270c27c2b329c413f3def71c5a9f
MaxKlaxxMiner <max@tetrisk.de>
2017-08-29 18:19:15 +0200

    direkt-anzeige der frames und ticks im diagramm

--- Research/Sync1/Form1.cs
+++ Research/Sync1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region # using *.*
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+#endregion
 
 namespace Sync1
 {
@@ -20,17 +22,49 @@ namespace Sync1
       InitializeComponent();
     }
 
+    const double TicksPerSecond = 5;
+    const double FramesPerSecond = 30;
+    const double MaxTime = 10.0;
+
     static readonly double[] TestValues = { 0.0, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9, 1.0, 0.8, 0.2, -0.6, -1.0, -0.9, -0.8, -0.6, -0.4, -0.2 };
     static readonly int TestCount = TestValues.Length;
-    static int testIndex;
+
+    static void DrawSync(Graphics g, float penSize)
+    {
+      const float CrossSize = 0.01f;
+
+      // --- Animationskurve zeichnen ---
+      var pBlue = new Pen(Color.CornflowerBlue, penSize);
+
+      int testIndex = 0;
+      var sync = new TickSync(TicksPerSecond, t =>
+      {
+        testIndex++;
+        if (testIndex == TestCount) testIndex = 0; // von vorne beginnen
+      });
+
+      const float FrameStep = (float)(1 / FramesPerSecond);
+      for (int frame = 0; frame * FrameStep < MaxTime; frame++)
+      {
+        float val = (float)TestValues[(int)(frame * TicksPerSecond / FramesPerSecond) % TestCount];
+        g.DrawLine(pBlue, frame * FrameStep - CrossSize, val - CrossSize, frame * FrameStep + CrossSize, val + CrossSize);
+        g.DrawLine(pBlue, frame * FrameStep - CrossSize, val + CrossSize, frame * FrameStep + CrossSize, val - CrossSize);
+      }
+
+
+      // --- Basis-Ticks als Rote Kreuze zeichnen ---
+      var pRed = new Pen(Color.DarkRed, penSize);
+
+      const float TickStep = (float)(1 / TicksPerSecond);
+      for (int tick = 0; tick * TickStep < MaxTime; tick++)
+      {
+        g.DrawLine(pRed, tick * TickStep - CrossSize, (float)TestValues[tick % TestCount] - CrossSize, tick * TickStep + CrossSize, (float)TestValues[tick % TestCount] + CrossSize);
+        g.DrawLine(pRed, tick * TickStep - CrossSize, (float)TestValues[tick % TestCount] + CrossSize, tick * TickStep + CrossSize, (float)TestValues[tick % TestCount] - CrossSize);
+      }
+    }
 
     void Draw()
     {
-      var sync = new TickSync(0.5, t =>
-      {
-        testIndex++;
-      });
-
       // --- Bild vorbereiten ---
       var pic = new Bitmap(pictureBox1.Width, pictureBox1.Height, PixelFormat.Format32bppRgb);
       var g = Graphics.FromImage(pic);
@@ -45,17 +79,17 @@ namespace Sync1
 
       // --- Diagramm-Basis zeichnen ---
       var pGray = new Pen(Color.Gray, 1f / scale);
-      g.DrawLine(pGray, 0f, 0f, 10f, 0f);
+      g.DrawLine(pGray, 0f, 0f, (float)MaxTime, 0f);
       g.DrawLine(pGray, 0f, -1f, 0f, +1f);
       var font = new Font("arial", scale * 0.08f / pic.Height);
       var bGray = new SolidBrush(Color.Gray);
 
-      for (int x = 10; x < 1000; x += 10)
+      for (int x = 10; x <= (int)(MaxTime * 100); x += 10)
       {
         float len = x % 100 == 0 ? 0.08f : x % 50 == 0 ? 0.04f : 0.02f;
         g.DrawLine(pGray, x * 0.01f, 0f, x * 0.01f, len);
         if (x % 50 != 0) continue;
-        string n = (x * 0.01f).ToString("0.0", CultureInfo.InvariantCulture).TrimEnd('0', '.');
+        string n = (x * 0.01f).ToString("0.0", CultureInfo.InvariantCulture).Replace(".0", "");
         var m = g.MeasureString(n, font);
         g.DrawString(n, font, bGray, x * 0.01f - m.Width * 0.5f, len + 0.01f);
       }
@@ -69,21 +103,12 @@ namespace Sync1
         g.DrawString(n, font, bGray, -m.Width - 0.08f, y * 0.01f - m.Height * 0.5f);
       }
 
-      // --- Matrix umdrehen ---
+      // --- Matrix umdrehen (-y) ---
       g.ResetTransform();
       g.TranslateTransform(scale * 0.2f, full);
       g.ScaleTransform(scale, -scale);
 
-      // --- Basis-Ticks als Rote Kreuze zeichnen ---
-      var pRed = new Pen(Color.DarkRed, 1f / scale);
-
-      for (int x = 0; x < 100; x++)
-      {
-        g.DrawLine(pRed, x * 0.1f - 0.01f, (float)TestValues[x % TestCount] - 0.01f, x * 0.1f + 0.01f, (float)TestValues[x % TestCount] + 0.01f);
-        g.DrawLine(pRed, x * 0.1f - 0.01f, (float)TestValues[x % TestCount] + 0.01f, x * 0.1f + 0.01f, (float)TestValues[x % TestCount] - 0.01f);
-      }
-
-      var pBlue = new Pen(Color.CornflowerBlue, 1f / scale);
+      DrawSync(g, 1f / scale);
 
       pictureBox1.Image = pic;
     }
